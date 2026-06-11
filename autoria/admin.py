22674c632253c8e5a755b326d4782913b3a36525
ТвоÑnikilodiym <nikitabaram@gmail.com>
2026-06-11 18:57:12 +0800

    commit

--- autoria/admin.py
+++ autoria/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Car
 
-# Register your models here.
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'model', 'year', 'price_usd', 'city')
+    search_fields = ('brand', 'model', 'city')
--- autoria/models.py
+++ autoria/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    TRANSMISSION_CHOICES = [
+        ('A', 'Automatic'),
+        ('M', 'Manual'),
+    ]
+
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField()
+
+    price_usd = models.PositiveIntegerField()
+    price_uah = models.PositiveIntegerField()
+
+    mileage = models.PositiveIntegerField()
+    transmission = models.CharField(max_length=1, choices=TRANSMISSION_CHOICES)
+
+    engine = models.CharField(max_length=50)
+    city = models.CharField(max_length=100, default="Unknown")
+
+    plate = models.CharField(max_length=20)
+    posted = models.CharField(max_length=50)
+
+    image = models.URLField() # або ImageField якщл захочу файли вставляти
+
+    def __str__(self):
+        return f"{self.brand} {self.model}"
