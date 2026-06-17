7c0896fcf317eb0c1091c584723ac1f619ab2c0c
nikilodiym <nikitabaram@gmail.com>
2026-06-17 22:18:04 +0800

    commit

--- autoria/urls.py
+++ autoria/urls.py
@@ -1,9 +1,28 @@
 from django.urls import path
 
-from .views import car_detail, index, login, sell_car
+from .views import (
+    car_detail,
+    car_services,
+    favorites,
+    garage,
+    index,
+    login,
+    new_cars,
+    search,
+    sell_car,
+    support,
+    used_cars,
+)
 
 urlpatterns = [
     path('', index, name='index'),
+    path('used-cars/', used_cars, name='used_cars'),
+    path('new-cars/', new_cars, name='new_cars'),
+    path('car-services/', car_services, name='car_services'),
+    path('garage/', garage, name='garage'),
+    path('support/', support, name='support'),
+    path('search/', search, name='search'),
+    path('favorites/', favorites, name='favorites'),
     path('car/<int:car_id>/', car_detail, name='car_detail'),
     path('sell/', sell_car, name='sell_car'),
     path('login/', login, name='login'),
--- autoria/views.py
+++ autoria/views.py
@@ -10,6 +10,42 @@ def index(request):
     return render(request, 'index/index.html', {'cars': cars})
 
 
+def used_cars(request):
+    cars = Car.objects.filter(year__lt=2024)
+    if not cars.exists():
+        cars = Car.objects.all()
+    return render(request, 'used_cars/used_cars.html', {'cars': cars})
+
+
+def new_cars(request):
+    cars = Car.objects.filter(year__gte=2024)
+    if not cars.exists():
+        cars = Car.objects.all()[:3]
+    return render(request, 'new_cars/new_cars.html', {'cars': cars})
+
+
+def car_services(request):
+    return render(request, 'car_services/car_services.html')
+
+
+def garage(request):
+    return render(request, 'garage/garage.html')
+
+
+def support(request):
+    return render(request, 'support/support.html')
+
+
+def search(request):
+    cars = Car.objects.all()
+    return render(request, 'search/search.html', {'cars': cars})
+
+
+def favorites(request):
+    cars = Car.objects.all()[:3]
+    return render(request, 'favorites/favorites.html', {'cars': cars})
+
+
 def car_detail(request, car_id):
     car = get_object_or_404(Car, pk=car_id)
     similar_cars = Car.objects.exclude(pk=car.pk)[:6]
